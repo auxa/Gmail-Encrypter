35a0ae48a99578fd6b10d64128a90b66629eac53
Joseph Fitzpatrick <jfitzpa1@tcd.ie>
2017-03-29 12:27:11 +0100

    Updates

---
+++ Database/tables.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users(
+	id int NOT NULL,
+	public_key varchar NOT NULL,
+	private_key varchar NOT NULL,
+);
--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-InboxSDK.load('1', 'Hello World!').then(function(sdk){
+InboxSDK.load('1.0', 'Encrypt').then(function(sdk){
 
 	// the SDK has been loaded, now do something with it!
 	sdk.Compose.registerComposeViewHandler(function(composeView){
@@ -8,7 +8,25 @@ InboxSDK.load('1', 'Hello World!').then(function(sdk){
 			title: "Encrypt",
 			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
 			onClick: function(event) {
-				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
+
+				var message = composeView.getTextContent();
+
+				var PassPhrase = "The Moon is a Harsh Mistress.";
+
+				// The length of the RSA key, in bits.
+				var Bits = 1024;
+
+				var MattsRSAkey = cryptico.generateRSAKey(PassPhrase, Bits);
+			//	console.log(MattsRSAkey);
+
+
+				var MattsPublicKeyString = cryptico.publicKeyString(MattsRSAkey);
+				var EncryptionResult = cryptico.encrypt(message, MattsPublicKeyString);
+				var hold = JSON.stringify(EncryptionResult);
+
+				event.composeView.insertTextIntoBodyAtCursor(hold.cipher);
+
+				console.log(EncryptionResult.toString())
 			},
 		});
 
