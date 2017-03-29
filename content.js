a9929ac8acbd48c7f8baef864aa00f77a0bb4b1d
Joseph Fitzpatrick <jfitzpa1@tcd.ie>
2017-03-29 13:52:49 +0100

    Update

--- content.js
+++ content.js
@@ -1,34 +1,39 @@
-InboxSDK.load('1.0', 'Encrypt').then(function(sdk){
+InboxSDK.load('2', 'Encrypt').then(function(sdk){
 
 	// the SDK has been loaded, now do something with it!
 	sdk.Compose.registerComposeViewHandler(function(composeView){
 
+		var PassPhrase = "Passphrase";
+		// The length of the RSA key, in bits.
+		var Bits = 1024;
+		var MattsRSAkey = cryptico.generateRSAKey(PassPhrase, Bits);
+		var MattsPublicKeyString = cryptico.publicKeyString(MattsRSAkey);
+
 		// a compose view has come into existence, do something with it!
 		composeView.addButton({
 			title: "Encrypt",
-			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
+			iconUrl: 'https://cdn1.iconfinder.com/data/icons/hawcons/32/698630-icon-114-lock-128.png',
 			onClick: function(event) {
 
 				var message = composeView.getTextContent();
 
-				var PassPhrase = "The Moon is a Harsh Mistress.";
-
-				// The length of the RSA key, in bits.
-				var Bits = 1024;
-
-				var MattsRSAkey = cryptico.generateRSAKey(PassPhrase, Bits);
-			//	console.log(MattsRSAkey);
-
-
-				var MattsPublicKeyString = cryptico.publicKeyString(MattsRSAkey);
-				var EncryptionResult = cryptico.encrypt(message, MattsPublicKeyString);
+				var EncryptionResult = cryptico.encrypt(message, MattsPublicKeyString, MattsRSAkey);
 
 				event.composeView.setBodyText(EncryptionResult.cipher);
 
-				console.log(EncryptionResult.cipher);
 			},
 		});
+		composeView.addButton({
+			title: "Decrypt",
+			iconUrl: 'https://cdn1.iconfinder.com/data/icons/hawcons/32/698630-icon-114-lock-128.png',
+			onClick: function(event) {
+				var message = composeView.getTextContent();
+				var Decrypt = cryptico.decrypt(message, MattsRSAkey);
+				event.composeView.setBodyText(Decrypt.plaintext);
 
+								console.log(Decrypt);
+							},
+						});
 	});
 
 });
