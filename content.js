e23817dc285c5a54bb0066d90723f53bb4d89973
Joseph Fitzpatrick <jfitzpa1@tcd.ie>
2017-04-01 12:59:42 +0100

    Working Finally

--- content.js
+++ content.js
@@ -15,15 +15,9 @@ InboxSDK.load('2', 'sdk_auxaE_cc24e4dfd8').then(function(sdk){
 		var body = '';
 		var sender ='';
 		var xer;
-
 		var el = document.createElement("div");
 		 xer = threadView.getMessageViews();
 
-		el.innerHTML = 'Hello world! ' + body + " " + sender;
-		threadView.addSidebarContentPanel({
-			title: 'Decrypt',
-			el: el
-		});
 		console.log(xer[0].getSender());
 		var senderEmail = sdk.User.getEmailAddress();
 		console.log(sender);
@@ -37,9 +31,10 @@ InboxSDK.load('2', 'sdk_auxaE_cc24e4dfd8').then(function(sdk){
 
 		first = ten.indexOf("</div><div") ;
 		ten = ten.substring(0, first);
-		ten = ten.replace(/<wbr>/g, "/");
+		ten = ten.replace(/<wbr>/g, "");
 		var vello;
 		var message = ten;
+		console.log(message);
 		//var Decrypt = cryptico.decrypt(message, MattsRSAkey);
 
 	var canDe =-1	//check if the email is actually encrpyed and we have the key to unlock it
@@ -48,20 +43,33 @@ InboxSDK.load('2', 'sdk_auxaE_cc24e4dfd8').then(function(sdk){
 				console.log('getting key for '+ resp.keys[i].id);
 				vello = resp.keys[i].pri_key;
 				console.log('logging private key ' + vello);
+				console.log(resp.keys[i].pub_key);
 				canDe =1;
+
 				break;
 			}
 		}
 		var Bits = 1024;
-		var myRSA = cryptico.generateRSAKey(vello, Bits);
-		console.log(myRSA);
-		var ans;
+
+		//var MattsRSAkey = cryptico.generateRSAKey(vello, Bits);
+		var myRSA = cryptico.generateRSAKey("Passphrase", Bits);
+			var MattsPublicKeyString = cryptico.publicKeyString(myRSA);
+			console.log(MattsPublicKeyString);
+			var ans;
 		if(canDe =1){
+			console.log(message)
 			 ans = cryptico.decrypt(message, myRSA);
+			 console.log(ans);
 		}
 
 
-		console.log(ans);
+		el.innerHTML = 'Decrypted Message: <br> <br>' + ans.plaintext + '<br> <br> The Message was ' + ans.signature;
+		threadView.addSidebarContentPanel({
+			title: 'Decrypt',
+			el: el
+		});
+
+
 
 
 	});
@@ -76,12 +84,12 @@ InboxSDK.load('2', 'sdk_auxaE_cc24e4dfd8').then(function(sdk){
 			onClick: function(event) {
 				var receiver =composeView.getToRecipients();
 				console.log(receiver[0].emailAddress);
-				var PassPhrase = "";
+				var pubKey;
 
 				for(var i =0; i< resp.keys.length; i++){
 					if(resp.keys[i] != null && resp.keys[i].id == receiver[0].emailAddress){
 						console.log('logging id '+ resp.keys[i].id);
-						PassPhrase = resp.keys[i].pub_key;
+						pubKey = resp.keys[i].pub_key;
 						console.log('logging publicKeyString ' +resp.keys[i].pub_key);
 						break;
 					}
@@ -97,7 +105,7 @@ InboxSDK.load('2', 'sdk_auxaE_cc24e4dfd8').then(function(sdk){
 			//	console.log(receiver);
 
 
-				var EncryptionResult = cryptico.encrypt(message, PassPhrase);
+				var EncryptionResult = cryptico.encrypt(message, pubKey);
 
 				event.composeView.setBodyText(EncryptionResult.cipher);
 
